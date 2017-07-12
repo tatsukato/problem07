2803e9db3883e4a508c34dd2daafd9ce5ba4fbf3
T.Kato <actself.02@gmail.com>
2017-07-13 00:20:43 +0900

    コメントを残しました

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -17,10 +17,17 @@ class User extends CI_Controller
         
         $_SESSION['is_login'] = $this->User_model->can_login($post);
         
+        //そもそもですが、User_modelからは、メールアドレスに紐づくレコードを取得するだけです。
+        //コントローラで、パスワードが一致しているか確認してください。
+        //一致していたら、セッションにtrueを入れます。
+        
         //$_SESSION['is_login']に ture or false を入れたあと
         //ここで Menber.php の index() を呼び出して一覧画面を
         //表示させるのでしょうか？
         
+        // リダイレクトをしましょう。
+        // コントローラから別のコントローラを呼ぶ事はしません。
+        // リダイレクトを覚えましょう。
     }
 }
 ?>
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -28,7 +28,11 @@ class User_model extends CI_Model
 
                     var_dump( $data );
                 }*/
-        
+                
+                //これはUserコントローラでやりましょう。
+                //それとemailとpasswordが一致しているか確認しても意味が無いです。
+                //ユーザが入力したパスワードとDBに保存されているパスワードが一致しているか
+                //確認します。
                 if($post['email'] == $user['passward']) {
                     return true;
                 }
